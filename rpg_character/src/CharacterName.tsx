6e5993f85bfb154bc8ecb4e70c6bbf748946d76f
xKeenx <vlad.gorozhankin@mail.ru>
2021-06-09 14:43:23 +0300

    CharacterName and Character base stats component

---
+++ rpg_character/src/CharacterName.tsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+import {ButtonGroup, Input, Button} from '@material-ui/core';
+
+
+function CharacterName() {
+    const [name, setName] = useState<string>("")
+
+
+    return (
+        <div>
+            <div>Введите имя персонажа</div>
+            <div>
+                <div>{name}</div>
+                <Input value={name}
+                       onChange={event => setName(event.target.value)}/>
+                <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+                    <Button>Изменить</Button>
+                </ButtonGroup>
+            </div>
+        </div>
+    );
+}
+
+export default CharacterName;
---
+++ rpg_character/src/CharacterParams.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
+import {Button} from "@material-ui/core";
+
+function CharacterParams() {
+    const [strength, setStrength] = useState<number>(0)
+    const [agility, setAgility] = useState<number>(0)
+    const [intelligence, setIntelligence] = useState<number>(0)
+    const [charisma, setCharisma] = useState<number>(0)
+    const [life, setLife] = useState<number>(3)
+
+
+    return (
+
+        <div>
+
+            <div>Сила : {strength}
+                <AddBoxIcon onClick={() => {
+                    setStrength(strength + 1)
+                    setLife(life + 1)
+                }}/>
+                <IndeterminateCheckBoxIcon onClick={() => {
+                    if (strength > 0) {
+                        setStrength(strength - 1)
+                        if (life > 0) setLife(life - 1)
+                    }
+                }}/>
+            </div>
+            <div>Ловкость : {agility}
+                <AddBoxIcon onClick={() => setAgility(agility + 1)}/>
+                <IndeterminateCheckBoxIcon onClick={() => agility === 0 ? agility : setAgility(agility - 1)}/>
+            </div>
+            <div>Интелект : {intelligence}
+                <AddBoxIcon onClick={() => setIntelligence(intelligence + 1)}/>
+                <IndeterminateCheckBoxIcon
+                    onClick={() => intelligence === 0 ? intelligence : setIntelligence(intelligence - 1)}/>
+            </div>
+            <div>Харизма : {charisma}
+                <AddBoxIcon onClick={() => setCharisma(charisma + 1)}/>
+                <IndeterminateCheckBoxIcon onClick={() => charisma === 0 ? charisma : setCharisma(charisma - 1)}/>
+            </div>
+            <div>Жизненная сила: {life} / {3 + strength}
+                <Button variant="contained" color="secondary" size = "small" onClick={() => life === 0 ? life : setLife(life - 1)}>
+                    Урон
+                </Button>
+            </div>
+            <div> Уклонение : {10 + agility}</div>
+            <div>Энергичность : {agility + intelligence}</div>
+
+        </div>
+    );
+}
+
+export default CharacterParams;

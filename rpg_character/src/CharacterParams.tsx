9cb2f032027d66c58808c53bf529c8d15e2e78a4
xKeenx <vlad.gorozhankin@mail.ru>
2021-06-10 14:13:29 +0300

    add Styles

--- rpg_character/src/CharacterParams.tsx
+++ rpg_character/src/CharacterParams.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import AddBoxIcon from '@material-ui/icons/AddBox'
 import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox'
 import { Button } from '@material-ui/core'
 import CharacterSkills from './CharacterSkills'
+import characterParamsStyle from './characterParamsStyle.module.scss'
 
 function CharacterParams() {
   const [strength, setStrength] = useState<number>(0)
@@ -13,51 +14,63 @@ function CharacterParams() {
   const [showChangeButton, setShowChangeButton] = useState<boolean>(false)
 
   return (
-    <div>
-      Характеристики персонажа:
+    <div className={characterParamsStyle.wrapperDiv}>
+      <div className={characterParamsStyle.wrapperDiv}> Характеристики персонажа: </div>
       {showChangeButton ? (
         <Button variant='contained' color='primary' onClick={() => setShowChangeButton(false)}>
           Изменить параметры
         </Button>
       ) : (
         <>
-          <div>
-            Сила : {strength}
-            <AddBoxIcon
-              onClick={() => {
-                setStrength(strength + 1)
-                setLife(life + 1)
-              }}
-            />
-            <IndeterminateCheckBoxIcon
-              onClick={() => {
-                if (strength > 0) {
-                  setStrength(strength - 1)
-                  if (life > 0) setLife(life - 1)
-                }
-              }}
-            />
+          <div className={characterParamsStyle.paramsNameAndDistribution}>
+            <div> Сила : {strength} </div>
+            <div>
+              <AddBoxIcon
+                onClick={() => {
+                  setStrength(strength + 1)
+                  setLife(life + 1)
+                }}
+              />
+              <IndeterminateCheckBoxIcon
+                onClick={() => {
+                  if (strength > 0) {
+                    setStrength(strength - 1)
+                    if (life > 0) setLife(life - 1)
+                  }
+                }}
+              />
+            </div>
           </div>
-          <div>
-            Ловкость : {agility}
-            <AddBoxIcon onClick={() => setAgility(agility + 1)} />
-            <IndeterminateCheckBoxIcon onClick={() => (agility === 0 ? agility : setAgility(agility - 1))} />
+          <div className={characterParamsStyle.paramsNameAndDistribution}>
+            <div> Ловкость : {agility} </div>
+            <div>
+              <AddBoxIcon onClick={() => setAgility(agility + 1)} />
+              <IndeterminateCheckBoxIcon onClick={() => (agility === 0 ? agility : setAgility(agility - 1))} />
+            </div>
           </div>
-          <div>
-            Интелект : {intelligence}
-            <AddBoxIcon onClick={() => setIntelligence(intelligence + 1)} />
-            <IndeterminateCheckBoxIcon
-              onClick={() => (intelligence === 0 ? intelligence : setIntelligence(intelligence - 1))}
-            />
+          <div className={characterParamsStyle.paramsNameAndDistribution}>
+            <div> Интелект : {intelligence} </div>
+            <div>
+              <AddBoxIcon onClick={() => setIntelligence(intelligence + 1)} />
+              <IndeterminateCheckBoxIcon
+                onClick={() => (intelligence === 0 ? intelligence : setIntelligence(intelligence - 1))}
+              />
+            </div>
           </div>
-          <div>
-            Харизма : {charisma}
-            <AddBoxIcon onClick={() => setCharisma(charisma + 1)} />
-            <IndeterminateCheckBoxIcon onClick={() => (charisma === 0 ? charisma : setCharisma(charisma - 1))} />
+          <div className={characterParamsStyle.paramsNameAndDistribution}>
+            <div>Харизма : {charisma} </div>
+            <div>
+              <AddBoxIcon onClick={() => setCharisma(charisma + 1)} />
+              <IndeterminateCheckBoxIcon onClick={() => (charisma === 0 ? charisma : setCharisma(charisma - 1))} />
+            </div>
           </div>
-          <div>
-            Жизненная сила: {life} / {3 + strength}
+          <div className={characterParamsStyle.paramsNameAndDistribution}>
+            <div>
+              {' '}
+              Жизненная сила: {life} / {3 + strength}{' '}
+            </div>
             <Button
+              className={characterParamsStyle.damageButton}
               variant='contained'
               color='secondary'
               size='small'
@@ -66,8 +79,8 @@ function CharacterParams() {
               Урон
             </Button>
           </div>
-          <div> Уклонение : {10 + agility}</div>
-          <div>Энергичность : {agility + intelligence}</div>
+          <div className={characterParamsStyle.paramsNameAndDistribution}> Уклонение : {10 + agility}</div>
+          <div className={characterParamsStyle.paramsNameAndDistribution}>Энергичность : {agility + intelligence}</div>
 
           <CharacterSkills strength={strength} agility={agility} intelligence={intelligence} charisma={charisma} />
           <Button variant='contained' color='primary' onClick={() => setShowChangeButton(true)}>
--- rpg_character/src/CharacterSkills.tsx
+++ rpg_character/src/CharacterSkills.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import AddBoxIcon from '@material-ui/icons/AddBox'
 import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox'
+import characterSkillsStyle from './characterSkillsStyles.module.scss'
 
 function CharacterSkills(params: { strength: number; agility: number; intelligence: number; charisma: number }) {
   const [attack, setAttack] = useState<number>(0)
@@ -61,112 +62,157 @@ function CharacterSkills(params: { strength: number; agility: number; intelligen
   }
 
   return (
-    <div>
+    <div className={characterSkillsStyle.wrapperDiv}>
       <div>
-        <img
-          src='https://cdn2.iconfinder.com/data/icons/rpg-fantasy-game-skill-ui/512/game_skill_ui_sword_attack-512.png'
-          alt={'attack'}
-          width='30'
-          height='30'
-        />
-        Атака : {attack}
-        Уровень : {levelName(attack)}
-        <AddBoxIcon onClick={() => levelAttackPlus(params.strength, attack)} />
-        <IndeterminateCheckBoxIcon onClick={() => (attack === 0 ? attack : setAttack(attack - 1))} />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img
+            src='https://cdn2.iconfinder.com/data/icons/rpg-fantasy-game-skill-ui/512/game_skill_ui_sword_attack-512.png'
+            alt={'attack'}
+            width='30'
+            height='30'
+          />
+          Атака : {attack}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}>Уровень : {levelName(attack)}</div>
+        <div>
+          <AddBoxIcon onClick={() => levelAttackPlus(params.strength, attack)} />
+          <IndeterminateCheckBoxIcon onClick={() => (attack === 0 ? attack : setAttack(attack - 1))} />
+        </div>
       </div>
       <div>
-        <img src='https://static.thenounproject.com/png/1223766-200.png' alt={'invisibility'} width='30' height='30' />
-        Стелс : {invisibility}
-        Уровень : {levelName(invisibility)}
-        <AddBoxIcon onClick={() => levelInvisibilityPlus(params.agility, invisibility)} />
-        <IndeterminateCheckBoxIcon
-          onClick={() => (invisibility === 0 ? invisibility : setInvisibility(invisibility - 1))}
-        />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img
+            src='https://static.thenounproject.com/png/1223766-200.png'
+            alt={'invisibility'}
+            width='30'
+            height='30'
+          />
+          Стелс : {invisibility}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}> Уровень : {levelName(invisibility)} </div>
+        <div>
+          <AddBoxIcon onClick={() => levelInvisibilityPlus(params.agility, invisibility)} />
+          <IndeterminateCheckBoxIcon
+            onClick={() => (invisibility === 0 ? invisibility : setInvisibility(invisibility - 1))}
+          />
+        </div>
       </div>
       <div>
-        <img
-          src='https://png.pngtree.com/png-clipart/20190517/original/pngtree-vector-bow-icon-png-image_3784384.jpg'
-          alt={'bow'}
-          width='30'
-          height='30'
-        />
-        Стрельба из лука : {bowShot}
-        Уровень : {levelName(bowShot)}
-        <AddBoxIcon onClick={() => levelBowShotPlus(params.agility, bowShot)} />
-        <IndeterminateCheckBoxIcon onClick={() => (bowShot === 0 ? bowShot : setBowShot(bowShot - 1))} />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img
+            src='https://png.pngtree.com/png-clipart/20190517/original/pngtree-vector-bow-icon-png-image_3784384.jpg'
+            alt={'bow'}
+            width='30'
+            height='30'
+          />
+          Стрельба из лука : {bowShot}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}>Уровень : {levelName(bowShot)}</div>
+        <div>
+          <AddBoxIcon onClick={() => levelBowShotPlus(params.agility, bowShot)} />
+          <IndeterminateCheckBoxIcon onClick={() => (bowShot === 0 ? bowShot : setBowShot(bowShot - 1))} />
+        </div>
       </div>
       <div>
-        <img src='https://img.icons8.com/ios/452/learning.png' alt={'learn'} width='30' height='30' />
-        Обучаемость : {learnability}
-        Уровень : {levelName(learnability)}
-        <AddBoxIcon onClick={() => levelLearnabilityPlus(params.intelligence, learnability)} />
-        <IndeterminateCheckBoxIcon
-          onClick={() => (learnability === 0 ? learnability : setLearnability(learnability - 1))}
-        />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img src='https://img.icons8.com/ios/452/learning.png' alt={'learn'} width='30' height='30' />
+          Обучаемость : {learnability}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}> Уровень : {levelName(learnability)} </div>
+        <div>
+          <AddBoxIcon onClick={() => levelLearnabilityPlus(params.intelligence, learnability)} />
+          <IndeterminateCheckBoxIcon
+            onClick={() => (learnability === 0 ? learnability : setLearnability(learnability - 1))}
+          />
+        </div>
       </div>
       <div>
-        <img
-          src='https://cdn.iconscout.com/icon/premium/png-512-thumb/survival-1579998-1337780.png'
-          alt={'survival'}
-          width='30'
-          height='30'
-        />
-        Выживание : {survival}
-        Уровень : {levelName(survival)}
-        <AddBoxIcon onClick={() => levelSurvivalPlus(params.intelligence, survival)} />
-        <IndeterminateCheckBoxIcon onClick={() => (survival === 0 ? survival : setSurvival(survival - 1))} />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img
+            src='https://cdn.iconscout.com/icon/premium/png-512-thumb/survival-1579998-1337780.png'
+            alt={'survival'}
+            width='30'
+            height='30'
+          />
+          Выживание : {survival}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}>Уровень : {levelName(survival)}</div>
+        <div>
+          <AddBoxIcon onClick={() => levelSurvivalPlus(params.intelligence, survival)} />
+          <IndeterminateCheckBoxIcon onClick={() => (survival === 0 ? survival : setSurvival(survival - 1))} />
+        </div>
       </div>
       <div>
-        <img
-          src='https://i.pinimg.com/736x/e4/11/bb/e411bb663600fdc02f64d72db214b964.jpg'
-          alt={'heal'}
-          width='30'
-          height='30'
-        />
-        Медицина : {heal}
-        Уровень : {levelName(heal)}
-        <AddBoxIcon onClick={() => levelHealPlus(params.intelligence, heal)} />
-        <IndeterminateCheckBoxIcon onClick={() => (heal === 0 ? heal : setHeal(heal - 1))} />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img
+            src='https://i.pinimg.com/736x/e4/11/bb/e411bb663600fdc02f64d72db214b964.jpg'
+            alt={'heal'}
+            width='30'
+            height='30'
+          />
+          Медицина : {heal}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}> Уровень : {levelName(heal)}</div>
+        <div>
+          <AddBoxIcon onClick={() => levelHealPlus(params.intelligence, heal)} />
+          <IndeterminateCheckBoxIcon onClick={() => (heal === 0 ? heal : setHeal(heal - 1))} />
+        </div>
       </div>
       <div>
-        <img src='https://static.thenounproject.com/png/3028821-200.png' alt={'fear'} width='30' height='30' />
-        Запугивание : {fear}
-        Уровень : {levelName(fear)}
-        <AddBoxIcon onClick={() => levelFearPlus(params.charisma, fear)} />
-        <IndeterminateCheckBoxIcon onClick={() => (fear === 0 ? fear : setFear(fear - 1))} />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img src='https://static.thenounproject.com/png/3028821-200.png' alt={'fear'} width='30' height='30' />
+          Запугивание : {fear}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}> Уровень : {levelName(fear)} </div>
+        <div>
+          <AddBoxIcon onClick={() => levelFearPlus(params.charisma, fear)} />
+          <IndeterminateCheckBoxIcon onClick={() => (fear === 0 ? fear : setFear(fear - 1))} />
+        </div>
       </div>
       <div>
-        <img
-          src='https://www.pngitem.com/pimgs/m/10-107005_data-insight-icon-png-transparent-png.png'
-          alt={'insight'}
-          width='30'
-          height='30'
-        />
-        Проницательность : {insight}
-        Уровень : {levelName(insight)}
-        <AddBoxIcon onClick={() => levelInsightPlus(params.charisma, insight)} />
-        <IndeterminateCheckBoxIcon onClick={() => (insight === 0 ? insight : setInsight(insight - 1))} />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img
+            src='https://www.pngitem.com/pimgs/m/10-107005_data-insight-icon-png-transparent-png.png'
+            alt={'insight'}
+            width='30'
+            height='30'
+          />
+          Проницательность : {insight}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}> Уровень : {levelName(insight)} </div>
+        <div>
+          <AddBoxIcon onClick={() => levelInsightPlus(params.charisma, insight)} />
+          <IndeterminateCheckBoxIcon onClick={() => (insight === 0 ? insight : setInsight(insight - 1))} />
+        </div>
       </div>
       <div>
-        <img
-          src='https://image.flaticon.com/icons/png/512/1100/1100571.png'
-          alt={'appearance'}
-          width='30'
-          height='30'
-        />
-        Внешний вид : {appearance}
-        Уровень : {levelName(appearance)}
-        <AddBoxIcon onClick={() => levelAppearancePlus(params.charisma, appearance)} />
-        <IndeterminateCheckBoxIcon onClick={() => (appearance === 0 ? appearance : setAppearance(appearance - 1))} />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img
+            src='https://image.flaticon.com/icons/png/512/1100/1100571.png'
+            alt={'appearance'}
+            width='30'
+            height='30'
+          />
+          Внешний вид : {appearance}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}>Уровень : {levelName(appearance)} </div>
+        <div>
+          <AddBoxIcon onClick={() => levelAppearancePlus(params.charisma, appearance)} />
+          <IndeterminateCheckBoxIcon onClick={() => (appearance === 0 ? appearance : setAppearance(appearance - 1))} />
+        </div>
       </div>
       <div>
-        <img src='https://static.thenounproject.com/png/1714268-200.png' alt={'bow'} width='30' height='30' />
-        Манипулирование : {manipulation}
-        Уровень : {levelName(manipulation)}
-        <AddBoxIcon onClick={() => levelManipulationPlus(params.charisma, manipulation)} />
-        <IndeterminateCheckBoxIcon
-          onClick={() => (manipulation === 0 ? manipulation : setManipulation(manipulation - 1))}
-        />
+        <div className={characterSkillsStyle.iconDiv}>
+          <img src='https://static.thenounproject.com/png/1714268-200.png' alt={'bow'} width='30' height='30' />
+          Манипулирование : {manipulation}
+        </div>
+        <div className={characterSkillsStyle.levelMargin}> Уровень : {levelName(manipulation)} </div>
+        <div>
+          <AddBoxIcon onClick={() => levelManipulationPlus(params.charisma, manipulation)} />
+          <IndeterminateCheckBoxIcon
+            onClick={() => (manipulation === 0 ? manipulation : setManipulation(manipulation - 1))}
+          />
+        </div>
       </div>
     </div>
   )
